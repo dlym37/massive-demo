2f8f06c13a5f793ce617f62a5f579f40dd066e71
Dano Lym <dlym37@gmail.com>
2017-12-14 13:21:29 -0700

    Demo complete

---
+++ db/getIncidentsByState.sql
@@ -0,0 +1,11 @@
+select
+incidents.id,
+state,
+injuries.name,
+affectedareas.name,
+causes.name as cause
+from incidents
+join injuries on incidents.injuryid = injuries.id
+join affectedareas on  injuries.affectedareaid = affectedareas.id
+join causes on incidents.causeid = causes.id
+where state = upper($1)
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
+
+const connectionString = 'postgres://ipifdlwyivkefi:e9303c08b640b48d86354b3d8ad385892bac48548e4385a47b015c0b6c79e64f@ec2-54-235-123-153.compute-1.amazonaws.com:5432/d370s34hjcbvr2?ssl=true';
+
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,17 +12,56 @@ app.use(bodyParser.json());
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('massive-demo');
+  const db = req.app.get('db');
+  db.getAllInjuries().then(injuries => {
+    res.send(injuries);
+  });
+
 });
 
 app.get('/incidents', (req, res) => {
-  res.send([]);
+  const db = req.app.get('db');
+  const state = req.query.state;
+  if (state) {
+    db.getIncidentsByState([state]).then(incidents => {
+      res.send(incidents);
+    })
+  } else {
+    db.getAllIncidents().then(incidents => {
+      res.send(incidents);
+    })
+  }
+
+
 });
 
 app.post('/incidents', (req, res) => {
-  res.send({id: 123});
+  const incident = req.body;
+  const db = req.app.get('db');
+  db.createIncident([
+    incident.state,
+    incident.injuryid,
+    incident.causeid
+  ]).then(results => {
+    res.send(results[0]);
+  });
 });
 
-app.listen(port, () => {
-  console.log('Started server on port', port);
+app.patch('/incidents/:id', (req, res) => {
+  const id = req.params.id;
+  const fields = req.body;
+  const db = req.app.get('db' );
+
+ db.updateIncidents([fields.state, id]).then(results => {
+    res.send(results);
+  });
 });
+
+massive(connectionString).then(db => {
+  app.set('db', db);
+  app.listen(port, () => {
+    console.log('Started server on port', port);
+  });
+});
+
+
